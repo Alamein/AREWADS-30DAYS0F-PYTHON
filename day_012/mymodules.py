223ed7e92b46b3ff2e7dfe8f81271b8e2e76235c
Aminu Hamza Nababa <alaminhnab4@gmail.com>
2023-11-10 21:22:06 +0100

    update

--- day_012/mymodules.py
+++ day_012/mymodules.py
@@ -1,4 +1,16 @@
-def addition(x, y):
-    return x + y
+def generate_fullname(firstname, lastname):
+    fullname = firstname + " " + lastname
+    return fullname
+
+
+def addition(num1, num2):
+    total = num1 + num2
+    return f'The summation of the given numbers is {total}'
+
+
+def weight_of_object(mass, acceleration):
+    weight = format(mass*acceleration, '.0f')
+    return weight
+print('The weight of object is: ', weight_of_object(100, 9.8), 'N', sep='')
 
 
